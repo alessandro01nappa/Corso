556b40bed4ced7e288405f0c4c37efb9fd33cb97
alessandro.nappa <alessandro01nappa@gmail.com>
2026-04-03 17:06:47 +0200

    Update script.js

--- Unita 2/3 aprile esame/script.js
+++ Unita 2/3 aprile esame/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => { // questo ho dovuto cercar
         .then(codice => {
             document.getElementById('box-animazione-m').innerHTML = codice
         }) // questa funzione non la conoscevo mi smebrava l'unica soluzione per evitare 3000 righe di html, l'ho cercata su internet
-
+// non capisco come mai il file io non lo avevo in svg ma in html, quindi ho dovuto importare un html anzichè un svg ho provato a trovare una soluzione come ho potuto non so se anche gli altri hanno avuto lo stesso problema
     // CAMBIO COLORE ALLO SCORRIMENTO
     const testata = document.getElementById('testata-principale')
     const hero = document.getElementById('hero')
